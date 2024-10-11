8e622a95e74e6ad1025c84a8ce31c4513c864649
Daniel Chen <dchen0058@gmail.com>
2024-10-11 12:59:21 +1100

    Added Footer Component

---
+++ src/components/BaseFooter.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <footer class="bg-white dark:bg-gray-900 m-4">
+    <div class="flex-1" />
+    <hr class="border-gray-200 dark:border-text-700 w-full mx-auto py-2" />
+    <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400"
+      >© 2024 Daniel Chen. All Rights Reserved.</span
+    >
+  </footer>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import Navbar from '@/components/BaseNavBar.vue'
+import Footer from '@/components/BaseFooter.vue'
 </script>
 
 <template>
   <Navbar />
   <main></main>
+  <Footer />
 </template>
