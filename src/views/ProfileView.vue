c2da42ddea757677131162ee22ab6f16228529a9
Daniel Chen <dchen0058@gmail.com>
2024-10-11 16:21:14 +1100

    Barebones profile page

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts">
 import BaseLayout from '@/components/BaseLayout.vue'
+
+import SUNGLASSES_EMOJI from '@/assets/img/Sunglasses-Emoji.png'
 </script>
 
 <template>
-  <BaseLayout> </BaseLayout>
+  <BaseLayout>
+    <div class="text-center w-full mt-8">
+      <img :src="SUNGLASSES_EMOJI" alt="Logo" class="h-64 w-64 rounded-full mx-auto" />
+      <p class="text-5xl mb-2">Daniel Chen</p>
+      <p class="text-xl">da.chen@alfred.org.au</p>
+    </div>
+  </BaseLayout>
 </template>
